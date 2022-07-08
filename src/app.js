a392268cb29e887c6d455c5bfd77ef1c71cdfe88
Jason <jkuffler@catalyte.io>
2022-07-07 23:06:22 -0400

    caught and corrected html layout

--- src/app.js
+++ src/app.js
@@ -1,15 +1,27 @@
-const resultsList = document.getElementById("results");
+const resultsList = document.getElementById("list-output");
 const volumeUrl = "https://www.googleapis.com/books/v1/volumes?q=";
 const aKey = "AIzaSyBxFPi1cP6NOlvllPSvAcGpZr0afgV19wk"
-const placeholder = document.createElement("IMG");
-placeholder.setAttribute("src","../resources/elena-joland-unsplash.jpg");
-placeholder.setAttribute("alt","Default Cover");
-let responseTime, authors, publishDates, coverImg, volumeLink;
-let totalResults;
+const placeholder = "<img src='../resources/elena-joland-unsplash.jpg>"
+let responseTime, authors, publishDates, publisher, contentLink, coverImg, volumeLink, totalResults;
+let searchResults;
 
-const search = document.getElementById("search")
+const searchTerms = document.getElementById("search")
 
 const getResults = function() {
   resultsList.innerHTML = "";
-  totalResults = search.val
+  searchResults = searchTerms.val
 }
+
+function renderResult(result) {
+
+}
+
+function getTotalResults() {
+fetch(volumeUrl)
+.then( r => r.json() )
+.then( searchResults => searchResults.forEach( result => renderResult(result) ))
+
+}
+// document.createElement("IMG");
+// placeholder.setAttribute("src","../resources/elena-joland-unsplash.jpg");
+// placeholder.setAttribute("alt","Default Cover");

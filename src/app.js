927ba31b69386937622bb7fe24e5b3d4d42f293d
Jason <jkuffler@catalyte.io>
2022-07-08 03:26:01 -0400

    next work on fixing searchParams to ensure we get searchResults

--- src/app.js
+++ src/app.js
@@ -1,41 +1,29 @@
 const aKey = "AIzaSyBxFPi1cP6NOlvllPSvAcGpZr0afgV19wk"
-const title = document.getElementById("#title");
-const resultsList = document.getElementById("list-output");
+let resultsList = document.getElementById("list-output");
 const volumeUrl = "https://www.googleapis.com/books/v1/volumes?q=";
 const placeholder = "<img src='../resources/elena-joland-unsplash.jpg' alt='Default Cover'>"
-const searchTerms = document.getElementById("#search")
+let volumeTitle, responseTime, authors, publishDates, publisher, coverImg, volumeLink, searchParams; 
 
-let volume, responseTime, authors, publishDates, publisher, coverImg, volumeLink, searchParams; 
+document.querySelector("button").addEventListener("click", getResults)
 
-searchTerms.addEventListener("click", getResults )
-
-const getResults = function() {
+function getResults () {
   resultsList.innerHTML = "";
-  searchParams = searchTerms.value;
+  searchParams = document.getElementById("#search-box").value;
   if(searchParams === "" || searchParams === null) {
     alert("Please enter valid search terms")
   } else {
     fetch( volumeUrl+searchParams )
     .then( res => res.json() )
-    .then (searchResults => searchResults.forEach( result => renderResult(result) ))
+    .then (searchResults => console.table(searchResults) )  /*searchResults.forEach( volume => renderVolume(volume) )*/
     }
 }
-console.log(searchResults)
+// console.log(searchResults)
 
-
-
-function renderResult(result) {
-  if (result === undefined || result === null) {
+function renderVolume(volume) {
+  if (volume === undefined || volume === null) {
     alert("No matches found for the provided search term(s)");
   } else {
     title.animate({'marginTop': '5px'}, 1000);
 
   }
-}
-
-function getTotalResults() {
-fetch(volumeUrl)
-.then( r => r.json() )
-.then( searchResults => searchResults.forEach( result => renderResult(result) ))
-
 }
